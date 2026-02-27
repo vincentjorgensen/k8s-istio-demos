4c34415611dcb5fb381e83b22b927da4e40d12f9
Vincent Jorgensen <vincentjorgensen@users.noreply.github.com>
2026-02-27 14:01:16 -0800

    move ambient ns label function to own app

---
+++ apps/app_ingress.sh
@@ -0,0 +1,17 @@
+function app_init_ingress {
+  if $INGRESS_ENABLED; then
+    $ITER_MC_1 exec_enmesh_ingress_namespace
+  fi
+}
+
+function exec_enmesh_ingress_namespace {
+  if ! is_create_mode; then
+    _k_label="-"
+  fi
+
+  if $AMBIENT_ENABLED; then
+    $DRY_RUN kubectl label namespace "$INGRESS_NAMESPACE"                      \
+    "istio.io/dataplane-mode${_k_label}"                                       \
+    --context "$GSI_CONTEXT" --overwrite
+  fi
+}
